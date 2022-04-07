a3f1aaed62f0be38f1f3c6313434c899654644c4
heaven <phalekara@gmail.com>
2022-04-07 11:57:40 +0300

    sayı sıralama
    kodluyoruz hafta 3
    www.patikadev

---
+++ sır.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class sır {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        int a, b, c;
+        System.out.print("1.sayıyı giriniz");
+        a = i.nextInt();
+        System.out.print("2.sayıyı giriniz");
+        b = i.nextInt();
+        System.out.print("3.sayıyı giriniz");
+        c = i.nextInt();
+        if (a > b && a > c) {
+            if (b > c) {
+                System.out.print((a + ">") + (b + ">") + c);
+            } else {
+                System.out.print((a + ">") + (c + ">") + b);
+            }}
+            else if(b > a && b > c) {
+                if (a > c) {
+                    System.out.print((b + ">") + (a + ">") + c);
+                } else {
+                    System.out.print((b + ">") + (c + ">") + a);
+                }}
+            else if(c > a && c > b) {
+                if (a > b) {
+                    System.out.print((c + ">") + (a + ">") + b);
+                } else {
+                    System.out.print((c + ">") + (b + ">") + a);
+                }
+
+    }
+}
+}
